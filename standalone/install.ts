928fbf19810da65e64aa4f42d97677ac9f3bf717
pact-dev <pact-dev@users.noreply.github.com>
2025-10-22 11:11:11 +0000

    fix: update standalone to 2.5.4

--- standalone/install.ts
+++ standalone/install.ts
@@ -1,7 +1,7 @@
 import chalk from 'chalk';
 
 // Get latest version from https://github.com/pact-foundation/pact-standalone/releases
-export const PACT_STANDALONE_VERSION = '2.5.3';
+export const PACT_STANDALONE_VERSION = '2.5.4';
 
 function makeError(msg: string): Error {
   return new Error(chalk.red(`Error while locating pact binary: ${msg}`));
